6fe1ef5750f9deda81c8bbda6fbc11c9f62b8655
msakong <msakong@bu.edu>
2022-05-03 20:47:15 -0400

    built patchify

--- utils/patchify.py
+++ utils/patchify.py
@@ -13,55 +13,93 @@ import matplotlib.pyplot as plt
 import random
 import os
 
-def patchify(input: np.ndarray, patch_size: int):
+def calc_steps(input_arr,step):
+    '''
+    Calculate how many patches will fit in the input_arr
+    '''
+    x = input_arr.shape[0]
+    y = input_arr.shape[1]
+    z = input_arr.shape[2]
+    if x%step==0:
+        x_num = x//step
+    else:
+        x_num = x//step+1
+    if y%step==0:
+        y_num = y//step
+    else:
+        y_num = y//step+1
+    if z%step==0:
+        z_num = y//step
+    else:
+        z_num = z//step+1
+    return x_num, y_num, z_num
+
+def patchify(input_arr: np.ndarray, patch_size: int, step: int):
     '''
     Intentionally create overlapping patches
-    to enhance predictions on edges
+    to enhance predictions along edges
     '''
-    x = input.shape[0]
-    y = input.shape[1]
-    z = input.shape[2]
-    x_num = x//patch_size
-    y_num = y//patch_size
-    z_num = z//patch_size
-    random_patches = np.zeros((128,patch_size,patch_size,patch_size))
+    x = input_arr.shape[0]
+    y = input_arr.shape[1]
+    z = input_arr.shape[2]
+    x_num, y_num, z_num = calc_steps(input_arr, step)
+    patches = []
     location_logs = dict()
     cnt=0
     for i in range(x_num):
         for j in range(y_num):
             for k in range(z_num):
-                random_patches[cnt,:,:,:]=input[i*patch_size:i*patch_size+patch_size,
-                                                  j*patch_size:j*patch_size+patch_size,
-                                                  k*patch_size:k*patch_size+patch_size]
-                location_logs[cnt]=(i*patch_size,
-                                    j*patch_size,
-                                    k*patch_size)
+                loc_x = i*step
+                loc_y = j*step
+                loc_z = k*step
+                patch = input_arr[loc_x:loc_x+patch_size,
+                              loc_y:loc_y+patch_size,
+                              loc_z:loc_z+patch_size]
+                if patch.shape == (patch_size,patch_size,patch_size):
+                    patches.append(patch)
+                else:
+                    # if patch is cutoff, because it is located near borders of the input
+                    temp = np.array(patch.shape) - np.array((patch_size, patch_size,patch_size))
+                    
+                    loc_x = loc_x+temp[0]
+                    loc_y = loc_y+temp[1]
+                    loc_z = loc_z+temp[2]
+                    patch = input_arr[loc_x:loc_x+patch_size,
+                                  loc_y:loc_y+patch_size,
+                                  loc_z:loc_z+patch_size]
+                    patches.append(patch)    
+                # Save location information of each patch
+                location_logs[cnt]=(loc_x,
+                                    loc_y,
+                                    loc_z)
                 cnt+=1
-    
-    for i in range(cnt,128):
-        rand_x = random.randint(0,x-patch_size-1)
-        rand_y = random.randint(0,y-patch_size-1)
-        rand_z = random.randint(0,z-patch_size-1)
-        patch = input[rand_x:rand_x+patch_size, rand_y:rand_y+patch_size, rand_z:rand_z+patch_size]
-        random_patches[i,:,:,:] = patch
-        location_logs[i]=(rand_x,rand_y,rand_z)
-    return random_patches, location_logs
 
-def unpatchify(input:np.ndarray, location_list: dict, orig_size:int,patch_size:int,num_classes:int):
-    orig_array = np.zeros((orig_size,orig_size,orig_size,num_classes))
-    for i in range(128):
-        x = location_list[i][0]
-        y = location_list[i][1]
-        z = location_list[i][2]
-        orig_array[x:x+patch_size,y:y+patch_size,z:z+patch_size,:] += input[i,:,:,:,:]
+    return patches, location_logs
+
+def unpatchify(input_arr:np.ndarray, location_list: dict, orig_size:int,patch_size:int,num_classes:int=0):
+    if num_classes==0:
+        orig_array = np.zeros((orig_size,orig_size,orig_size))
+        for i in range(len(location_list)):
+            x = location_list[i][0]
+            y = location_list[i][1]
+            z = location_list[i][2]
+            orig_array[x:x+patch_size,y:y+patch_size,z:z+patch_size] += input_arr[i,:,:,:]
+    else:
+        orig_array = np.zeros((orig_size,orig_size,orig_size,num_classes))
+        for i in range(location_list):
+            x = location_list[i][0]
+            y = location_list[i][1]
+            z = location_list[i][2]
+            orig_array[x:x+patch_size,y:y+patch_size,z:z+patch_size,:] += input_arr[i,:,:,:,:]
     return orig_array
 
 if __name__=='__main__':
     #Testing Purpose
     
-    #test = np.ones((192,192,192))
-    #one, two = random_patchify(test,64)
-    #orig = unpatchify_to_original(one,two,192,64)
+    test = np.ones((10,10,10))
+    one, two = patchify(test,5,3)
+    oone = np.array(one)
+    orig = unpatchify(oone,two,10,5,0)
 
 
 
